e5284d856a95cc7e204dae09bc80fae495cfd60c
andrealoda <andrea@casaloda.it>
2026-03-14 21:40:54 +0100

    add on/off  logic in js file

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,16 @@
 const bulb = document.getElementById('image');
-
 const button = document.getElementById('button');
 
 button.addEventListener('click',
 
     function () {
-        bulb.src = './assets/img/yellow_lamp.png';
-        button.innerHTML = 'Turn off the light';
+        if (button.innerHTML.includes('on')) {
+            bulb.src = './assets/img/yellow_lamp.png';
+            button.innerHTML = 'Turn off the light';
+        } else {
+            bulb.src = './assets/img/white_lamp.png';
+            button.innerHTML = 'Turn on the light';
+        }
     }
 )
 
